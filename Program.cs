e3991fbcb3e361acb775bc048081070a843bf6cc
Mikhail Volkov <mikellevolkov@gmail.com>
2022-05-27 21:49:50 +0300

    suffering

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Generic;
+using System;
 
-Console.WriteLine("Hello, World!");
+class Nest
+{
+    public List<Unit> units;
+    public Nest(int countUnits, int totalDefense)
+    {
+        units = new List<Unit>(countUnits);
+    }
+
+    public void info()
+    {
+        while() {
+            Unit.info();
+        }
+    }
+}
+
+class Unit
+{
+    public int health;
+    public int damage;
+    public int defense;
+    public string UnitType;
+
+    public static void info()
+    {
+        Console.WriteLine("Здоровье = {health}, Урон = {damage}");
+    }
+}
+
+class HarvesterAnt : Unit
+{
+    public void harvest();
+}
+class WarriorAnt : Unit
+{
+    public void attack();
+}
